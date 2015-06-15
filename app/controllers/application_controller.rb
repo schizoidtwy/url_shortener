3c3c885725269b99d96f7e7bc32611c821f58fb2
Alexandre <alexandre.cruzeiro90@gmail.com>
2015-06-15 17:24:37 -0300

    terminando views por enquanto --- mexi nos controllers para pegar os parametros corretamente

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  def new
-    @short_url = Url.new
-  end
-  def create
-    @short_url = Url.new(params[:url])
-    if @short_url.save
-      flash[:short_id] = @short_url.id
-      redirect_to new_url_url
-    else
-      render :action => 'new'
-    end
-  end
-  def show
-    @short_url = Url.find(params [:id])
-    redirect_to @short_url.url
-  end
+  
 end
--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,4 +1,18 @@
 class UrlsController < ApplicationController
   def new
+    @url = Url.new
+  end
+  def create
+    @url = Url.new(params.require(:url).permit(:url))
+    respond_to do |format|
+      if @url.save
+          format.html {redirect_to @url, notice: 'Criado com sucesso'}
+      else
+          format.html{ render :new }
+      end
+    end
+  end
+  def show
+    @url = Url.all
   end
 end
